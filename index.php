0104ebb6118f76680f2ed05a8b1fe5913490edd5
Kausinho <kausinhobrunow@gmail.com>
2021-01-09 19:01:06 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Loja Virtual</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="estilo/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+	<link href="estilo/style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+	<header>
+		<div class="logo left"><a href="">Logomarca</a></div><!--logo-->
+			<nav class="desktop-right">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Produtos</a></li>
+					<li><a href="">Carrinho</a></li>
+					<li><a href="">Contato</a></li>
+				</ul>
+			</nav>
+	</header>
+</body>
+</html>
